eb023316287e911f45cdfa31117e030344df4852
Alan Zhu <alan.zhu39@gmail.com>
2022-10-16 06:34:38 -0700

    Initial Commit

---
+++ Time Tracker/Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_TrackerApp.swift
+//  Time Tracker
+//
+//  Created by Alan Zhu Personal on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
