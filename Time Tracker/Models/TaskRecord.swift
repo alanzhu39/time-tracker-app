a975ae2e2f79f5e0940ba1bcaf17450b942b3f71
Alan Zhu <alan.zhu39@gmail.com>
2022-10-16 13:57:35 -0700

    working on task records

--- Time Tracker/Models/TaskRecord.swift
+++ Time Tracker/Models/TaskRecord.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct TaskRecord {
+    var taskId: UUID
+    var dateInterval: DateInterval
+    var timeInterval: TimeInterval
+}
+
+extension TaskRecord {
+    static let sampleData: [TaskRecord] =
+    [
+        TaskRecord(taskId: UUID(), dateInterval: DateInterval(), timeInterval: TimeInterval())
+    ]
+}
